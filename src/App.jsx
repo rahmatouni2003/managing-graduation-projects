881c8a1ae259959db003ba9cd4b1f140a3a600fd
rehabhosney <rehabhosneymabeed@gmail.com>
2026-02-01 11:43:33 +0200

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div className="text-purple-600 text-4xl font-bold">rehab</div>
-    </>
-  );
+  return <></>;
 }
-
 export default App;
